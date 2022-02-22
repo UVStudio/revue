f718d8da0b05bd6dc1338c6cbbd3e71533076fa4
Leonard Shen <uvstudio2199@gmail.com>
2022-02-22 10:47:10 -0500

    getUser

--- api/getUser.js
+++ api/getUser.js
@@ -1,7 +1,6 @@
 /**
- * Route: POST /addUser
+ * Route: GET /getUser
  */
-const { v4: uuidv4 } = require('uuid');
 const AWS = require('aws-sdk');
 AWS.config.update({ region: 'us-east-2' });
 const timestamp = Date.now();
@@ -9,28 +8,26 @@ const timestamp = Date.now();
 const dynamoDB = new AWS.DynamoDB.DocumentClient();
 const tableName = process.env.USERS_TABLE;
 
-module.exports.addUser = async (event, context) => {
+module.exports.getUser = async (event, context) => {
   try {
-    const item = JSON.parse(event.body).item;
-    const headers = event.headers;
-    item.user_email = headers.user_email;
-    item.user_id = uuidv4();
-    item.timestamp = timestamp;
+    const userId = decodeURIComponent(event.pathParameters.user_id);
+    const params = {
+      TableName: tableName,
+      IndexName: 'user_id-index',
+      KeyConditionExpression: 'user_id = :user_id',
+      ExpressionAttributeValues: {
+        ':user_id': userId,
+      },
+      Limit: 1,
+    };
 
-    await dynamoDB
-      .put({
-        TableName: tableName,
-        Item: item,
-      })
-      .promise();
+    const data = await dynamoDB.query(params).promise();
 
     return {
       statusCode: 200,
       body: JSON.stringify({
         message: 'Go Serverless v3.0! Your function executed successfully!',
-        headers,
-        item,
-        event,
+        data,
       }),
     };
   } catch (err) {
